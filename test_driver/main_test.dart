5b9c987b22f2b49ba1e6b259e564344fd6fa0d0f
Nitesh Sharma <nitesh.sharma@joshtechnologygroup.com>
2022-06-10 19:39:50 +0530

    Initial Commit

---
+++ test_driver/main_test.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Initial App Test', () {
+    FlutterDriver? driver;
+    setUpAll(() async {
+      driver = await FlutterDriver.connect();
+    });
+
+    test('verify the text on home screen', () async {
+      SerializableFinder message = find.text("You have pushed the button this many times:");
+      await driver?.waitFor(message);
+      expect(await driver?.getText(message), "You have pushed the button this many times:");
+    });
+
+    tearDownAll(() {
+      driver?.close();
+    });
+  });
+}
