09546dc94795a3aad5b07fca968ab9777a0bf349
Nitesh Sharma <nitesh.sharma@joshtechnologygroup.com>
2022-06-10 21:11:08 +0530

    Date update

--- test_driver/main_test.dart
+++ test_driver/main_test.dart
@@ -13,7 +13,7 @@ void main() {
     test('verify the text on home screen', () async {
       SerializableFinder message = find.text("You have pushed the button this many times:");
       await driver?.waitFor(message);
-      final File image = await File('screenshots/${DateTime.now()}.png').create(recursive: true);
+      final File image = await File('screenshots/${DateTime.now().millisecondsSinceEpoch}.png').create(recursive: true);
 
       image.writeAsBytesSync(await driver!.screenshot());
       expect(await driver?.getText(message), "You have pushed the button this many times:");
